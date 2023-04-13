52518c44d6945f76fdd2da0bbbdb0eb34dcc6487
Diego Fernandes <diego.schell.f@gmail.com>
2023-04-13 15:03:42 -0300

    test

---
+++ build/js/variables.js
@@ -0,0 +1,200 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 13 Apr 2023 15:55:11 GMT
+ */
+
+module.exports = {
+  "color": {
+    "accent": {
+      "background": {
+        "description": "",
+        "type": "color",
+        "value": "#07020d",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:81eea1a748d75a11cfe4bb23ebabad24fc8d1e62,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "input/tokens.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#07020dff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:81eea1a748d75a11cfe4bb23ebabad24fc8d1e62,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "ColorAccentBackground",
+        "attributes": {
+          "category": "color",
+          "type": "accent",
+          "item": "background"
+        },
+        "path": [
+          "color",
+          "accent",
+          "background"
+        ]
+      },
+      "white": {
+        "description": "",
+        "type": "color",
+        "value": "#e6e6e7",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:4c14504080a3b8e452edd9ddf186103a2a81440f,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "input/tokens.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#e6e6e7ff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:4c14504080a3b8e452edd9ddf186103a2a81440f,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "ColorAccentWhite",
+        "attributes": {
+          "category": "color",
+          "type": "accent",
+          "item": "white"
+        },
+        "path": [
+          "color",
+          "accent",
+          "white"
+        ]
+      }
+    },
+    "purple": {
+      "300": {
+        "description": "",
+        "type": "color",
+        "value": "#39353d",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:837e51c40a8f68cf2af26f4648f068a8a994cac6,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "input/tokens.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#39353dff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:837e51c40a8f68cf2af26f4648f068a8a994cac6,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "ColorPurple300",
+        "attributes": {
+          "category": "color",
+          "type": "purple",
+          "item": "300"
+        },
+        "path": [
+          "color",
+          "purple",
+          "300"
+        ]
+      },
+      "400": {
+        "description": "",
+        "type": "color",
+        "value": "#201b25",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:e7b82863c242603ba951ef894a2adf4e6a5a4ccf,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "input/tokens.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#201b25ff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:e7b82863c242603ba951ef894a2adf4e6a5a4ccf,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "ColorPurple400",
+        "attributes": {
+          "category": "color",
+          "type": "purple",
+          "item": "400"
+        },
+        "path": [
+          "color",
+          "purple",
+          "400"
+        ]
+      }
+    },
+    "gray": {
+      "400": {
+        "description": "",
+        "type": "color",
+        "value": "#9c9a9e",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:9e3c576fe67ff48018a0726a5513613fa4fac175,",
+            "exportKey": "color"
+          }
+        },
+        "filePath": "input/tokens.json",
+        "isSource": true,
+        "original": {
+          "description": "",
+          "type": "color",
+          "value": "#9c9a9eff",
+          "blendMode": "normal",
+          "extensions": {
+            "org.lukasoppermann.figmaDesignTokens": {
+              "styleId": "S:9e3c576fe67ff48018a0726a5513613fa4fac175,",
+              "exportKey": "color"
+            }
+          }
+        },
+        "name": "ColorGray400",
+        "attributes": {
+          "category": "color",
+          "type": "gray",
+          "item": "400"
+        },
+        "path": [
+          "color",
+          "gray",
+          "400"
+        ]
+      }
+    }
+  }
+};
